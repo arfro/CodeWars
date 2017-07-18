73c6b1aa7ee0712cb00b459a0a3c04788312e77f
arfro <aerfromenes@gmail.com>
2017-07-18 14:52:09 +0100

    Update DigitalRoot_JUnit.java

--- DigitalRoot_JUnit.java
+++ DigitalRoot_JUnit.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.assertEquals;
 public class DigitalRoot_JUnit {
     @Test
     public void Tests() {
-      assertEquals( 7, DRoot.digital_root(16));
-      assertEquals( 6, DRoot.digital_root(6666));
-      assertEquals( 4, DRoot.digital_root(12100));
+      assertEquals( 7, DigitalRoot.digital_root(16));
+      assertEquals( 6, DigitalRoot.digital_root(6666));
+      assertEquals( 4, DigitalRoot.digital_root(12100));
     }
 }
