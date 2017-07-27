436264ec3973c296e35c20950d4fbbfe5497e953
Annette <aerfromenes@gmail.com>
2017-07-27 19:05:09 +0100

    Update ThreeStrings.java

--- ThreeStrings.java
+++ ThreeStrings.java
@@ -4,9 +4,7 @@ public class ThreeStrings {
     StringBuilder result = new StringBuilder();
     
     for(int i = 0; i < one.length(); i++){
-      result.append(one.substring(i,i+1));
-      result.append(two.substring(i,i+1));
-      result.append(three.substring(i,i+1));
+      result.append(one.substring(i,i+1)).append(two.substring(i,i+1)).append(three.substring(i,i+1));
     }
     
     return new String(result);
