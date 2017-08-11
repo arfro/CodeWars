896f0e360a9bdad414b71cddeb02cf6638f02a79
Annette <aerfromenes@gmail.com>
2017-08-11 15:21:29 +0100

    Update EnigmaMachine_Plugboard.java

--- EnigmaMachine_Plugboard.java
+++ EnigmaMachine_Plugboard.java
@@ -1,3 +1,28 @@
+/*
+The Enigma Machine was a message enciphering/deciphering machine used during the Second World War for disguising the content of military communications. Alan Turing - the father of computing - formulated and developed concepts that are the basis of all computers in use today, he did this in response to the vital need to break those military communications. Turing and his colleagues at Bletchley Park are generally recognised as being responsible for shortening WWII by two years and saving an estimated 22 Million lives.
+
+The Enigma Machine consisted of a number of parts: Keyboard for input, rotors and plugboard for enciphering, and lampboard for output.
+
+This class implements - PLUGBOARD 
+
+Physical Description
+
+The plugboard crosswired the 26 letters of the latin alphabet togther, so that an input into one letter could generate output as another letter. If a wire was not present, then the input letter was unchanged. Each plugboard came with a maximum of 10 wires, so at least six letters were not cross-wired.
+
+For example:
+
+If a wire connects A to B, then an A input will generate a B output and a B input will generate an A output.
+If no wire connects to C, then only a C input will generate a C output.
+Note
+
+In the actual usage of the original Enigma Machine, punctuation was encoded as words transmitted in the stream, in our code, anything that is not in the range A-Z will be returned unchanged.
+
+Take a list of wire pairs at construction in the form of a string, with a default behaviour of no wires configured. E.g. "ABCD" would wire A <-> B and C <-> D.
+Validate that the wire pairings are legitimate. Raise an exception if not.
+Implement the process method to translate a single character input into an output.
+
+*/
+
 import java.util.HashMap;
 import java.util.Map;
 
