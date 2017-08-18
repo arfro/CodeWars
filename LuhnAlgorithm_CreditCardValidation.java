06f91284cc6b6da22bdcd45f5093c6391755c350
Annette <aerfromenes@gmail.com>
2017-08-18 15:50:46 +0100

    Update LuhnAlgorithm_CreditCardValidation.java
    algorithm fixed to something much easier , was bugging me the whole night hehe

--- LuhnAlgorithm_CreditCardValidation.java
+++ LuhnAlgorithm_CreditCardValidation.java
@@ -1,47 +1,29 @@
-import java.util.ArrayList;
-
-public class LuhnAlgorithm_CreditCardValidation{
+public class Validate{
   public static boolean validate(String n){
-    ArrayList<Integer> numArr = new ArrayList<>();
-    //"result" variable will be used for the final sum
+  
     int result = 0;
+    boolean multiply = false;
     
     //edge cases
     if(n.length() == 0) return false;
     
-    //add each number in the string to an array of integers
-    for(int i=0; i < n.length(); i++) numArr.add(Integer.parseInt(n.substring(i, i+1)));
-    
-    //if there is an even amt of numbers get each second value starting from zero and... 
-    if(n.length()%2 == 0){
-      for(int i=0; i < numArr.size(); i=i+2){
-        //... multiply by 2.
-        int currentNr = Integer.valueOf(numArr.get(i)) * 2;
-        //if after multiplication the number has two digits, subtract 9...
-        if(currentNr > 9) currentNr = currentNr-9;
-        //and set as a new value
-        numArr.set(i, currentNr);
-      }
+    // Starting from the last digit if 'multiply' flag is true, multiply, add to result, toggle flag 
+    // If flag is false, just add to result
+    for(int i=n.length()-1; i >=0; i--){
+      //'0' has a value of 48. '1' is 49. That way if we subtract '0' which in reality is 48 we get the required int value 
+      int currentDigit = n.charAt(i) - '0';
+      if(multiply){
+        if((currentDigit * 2) < 9)
+          currentDigit = currentDigit * 2;
+          
+        else
+          currentDigit = (currentDigit * 2) - 9;
+       }
+      result += currentDigit;
+      multiply = !multiply;
     }
-    //if there is an uneven amt of numbers get each second value starting from one and... 
-    else{
-      for(int i=1; i < numArr.size(); i=i+2){
-       //... multiply by 2.
-        int currentNr = Integer.valueOf(numArr.get(i)) * 2;
-        //if after multiplication the number has two digits, subtract 9...
-        if(currentNr > 9) currentNr = currentNr-9;
-        //and set as a new value
-        numArr.set(i, currentNr);
-        System.out.println(numArr.get(i));
-      }
-    }
-    
-    //add all digits from the array
-    for(int nr : numArr) result += nr;
     
     if(result%10 == 0) return true;
     else return false;
-    
-    
   }
 }
