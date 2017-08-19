1086d027a45974743274f8ee57a9cfa5b9b33bd5
Annette <aerfromenes@gmail.com>
2017-08-19 08:58:18 +0100

    Update CamelCase_JUnit.java

--- CamelCase_JUnit.java
+++ CamelCase_JUnit.java
@@ -7,25 +7,25 @@ public class CamelCase_JUnit {
     public void testSomeUnderscoreLowerStart() {
       String input = "the_Stealth_Warrior";
       System.out.println("input: "+input);      
-      assertEquals("theStealthWarrior", Solution.toCamelCase(input));
+      assertEquals("theStealthWarrior", CamelCase.toCamelCase(input));
     }
     @Test
     public void testSomeDashLowerStart() {
       String input = "the-Stealth-Warrior";
       System.out.println("input: "+input);      
-      assertEquals("theStealthWarrior", Solution.toCamelCase(input));
+      assertEquals("theStealthWarrior", CamelCase.toCamelCase(input));
     }
     @Test
     public void testSomeDashLowerStart2() {
       String input = "the-shtelt-Warrior";
       System.out.println("input: "+input);      
-      assertEquals("theShteltWarrior", Solution.toCamelCase(input));
+      assertEquals("theShteltWarrior", CamelCase.toCamelCase(input));
     }
     
     @Test
     public void testEmpty() {
       String input = "";
       System.out.println("input: "+input);      
-      assertEquals("", Solution.toCamelCase(input));
+      assertEquals("", CamelCase.toCamelCase(input));
     }
 }
