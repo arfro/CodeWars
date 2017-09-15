ad7056e2bc07fdba89253b72498a2708e3c27e05
Annette <aerfromenes@gmail.com>
2017-09-15 22:12:40 +0100

    fixing typo

--- BraceChecker_JUnit.java
+++ BraceChecker_JUnit.java
@@ -13,26 +13,26 @@ public class BraceChecker_JUnit {
   @Test
 	public void BraceChecker_isValid_AllClosing() {
     assertEquals(false, checker.isValid(")))))"));
-	}}
+	}
   
   @Test
 	public void BraceChecker_isValid_Zipped() {
     assertEquals(false, checker.isValid("(])]"));
-	}}
+	}
   
   @Test
 	public void BraceChecker_isValid_Correct() {
     assertEquals(true, checker.isValid("{([[[]]])}"));
-	}}
+	}
   
   @Test
 	public void BraceChecker_isValid_Single() {
     assertEquals(false, checker.isValid("{"));
-	}}
+	}
   
    @Test
 	public void BraceChecker_isValid_SingleClosing() {
     assertEquals(false, checker.isValid(")"));
-	}}
+	}
 
 }
