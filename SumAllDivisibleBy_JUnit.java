792976885182d581811e6695ca493fc323089a98
Annette <aerfromenes@gmail.com>
2017-09-15 22:44:05 +0100

    Create SumAllDivisibleBy_JUnit.java

---
+++ SumAllDivisibleBy_JUnit.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SumAllDivisibleBy_JUnit {
+    @Test
+    public void SumAllDivisibleBy_solution_normalCase() {
+      assertEquals(23, new Solution().solution(10));
+    }
+    
+    @Test
+    public void () SumAllDivisibleBy_solution_Zero(){
+      assertEquals(0, new Solution().solution(0));
+    }
+    
+    @Test
+    public void () SumAllDivisibleBy_solution_Negative(){
+      assertEquals(0, new Solution().solution(-1));
+    }
+    
+}
