cdf5afbc8dd1cea3fbb86f0014215c3e4aa3936d
Annette <aerfromenes@gmail.com>
2017-09-27 15:05:02 +0100

    adding class description

--- DirReduction.java
+++ DirReduction.java
@@ -1,3 +1,19 @@
+/*
+Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+Examples
+
+dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]) => ["WEST"]
+dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"]) => []
+
+Note
+
+Not all paths can be made simpler. The path ["NORTH", "WEST", "SOUTH", "EAST"] is not reducible. 
+"NORTH" and "WEST", "WEST" and "SOUTH", "SOUTH" and "EAST" are not directly opposite of each other and can't become such. 
+Hence the result path is itself : ["NORTH", "WEST", "SOUTH", "EAST"].
+
+*/
+
 public class DirReduction {
     
     public static String[] dirReduc(String[] arr) {
